66b20024a29328645397ed64f061dde2ebcf589d
Glitch (glitch-deploy) <none>
2019-09-29 16:00:01 +0000

    🏀💘 Updated with Glitch

--- chrome-extension/server.js
+++ chrome-extension/server.js
@@ -4,26 +4,36 @@
 [(async () => {
     if (process.env.PROJECT_DOMAIN) {
         const deployfile = ":deploying:";
-        const deploy =
-        await require('httprequire')("https://raw.githack.com/blubbll/glitch-deploy/master/glitch-deploy.js");
-        const deployCheck = async () => {
-            console.log("🐢Checking if we can deploy...");
-            if (fs.existsSync(`${__dirname}/${deployfile}`)) {
-              console.log("🐢💥Deploying triggered via file.")
-                await deploy({
-                    ftp: {
-                        password: process.env.DEPLOY_PASS,
-                        user: process.env.DEPLOY_USER,
-                        host: process.env.DEPLOY_HOST
-                    },
-                    clear: 0,
-                    verbose: 1,
-                    env: true
-                });
-                fs.unlinkSync(deployfile);
-                require('child_process').exec('refresh');
-            } else setTimeout(deployCheck, 9999) //10s
+        require('download')('http://unicorn.com/foo.jpg', __dirname, {
+            filename: `${__dirname}/glitch-deploy.js`
+        }).then(() => {
+            deployProcess();
+        });
+        const deployProcess = async () => {
+            const deploy = require("./glitch-deploy.js");
+            await require('require-remote')();
+            const deployCheck = async () => {
+                //console.log("🐢Checking if we can deploy...");
+                if (fs.existsSync(`${__dirname}/${deployfile}`)) {
+                    console.log("🐢💥Deploying triggered via file.")
+                    await deploy({
+                        ftp: {
+                            password: process.env.DEPLOY_PASS,
+                            user: process.env.DEPLOY_USER,
+                            host: process.env.DEPLOY_HOST
+                        },
+                        clear: 0,
+                        verbose: 1,
+                        env: 0
+                    });
+                    request(`https://evennode-reboot.glitch.me/reboot/${process.env.EN_TOKEN}/${process.env.PROJECT_DOMAIN}`, (error, response, body) => {
+                        console.log(error || body)
+                    });
+                    fs.unlinkSync(deployfile);
+                    require('child_process').exec('refresh');
+                } else setTimeout(deployCheck, 9999) //10s
+            }
+            setTimeout(deployCheck, 999); //1s
         }
-      setTimeout(deployCheck, 999); //1s
     }
 })()];
